5ae513b9a735ca89a81c9abe52ef11eafe702864
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2023-08-20 11:22:35 +0530

    import java.util.Scanner; public class k{     public static void main(String[] args)     {         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         int a[]=new int[n];         int i;         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int sum1=0;         for(i=0;i<n;i++)         {             if(a[i]%2==0)             {               sum1+=a[i]  ;             }         }
            int sum2=0;
            for(i=0;i<n;i++)
            {
                if(a[i]%2!=0)
                {
                    sum2+=a[i];
                }
            }
            
             int ad=sum2-sum1;
             System.out.println(Math.abs(ad));
                    
        }
    }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class k{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int i;
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int sum1=0;
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+              sum1+=a[i]  ;
+            }
+        }
+       
+        int sum2=0;
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2!=0)
+            {
+                sum2+=a[i];
+            }
+        }
+        
+         int ad=sum2-sum1;
+         System.out.println(Math.abs(ad));
+                
+    }
+}
