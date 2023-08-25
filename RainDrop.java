aea1f10e183563d613f306e8dc57b138eff24d50
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2023-08-25 12:02:59 +0530

    import java.util.Scanner; public class k{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();
              if(n%3==0&&n%5==0&&n%7!=0)
             {
                 System.out.println("PlingPlang"); 
             }
             else if(n%5==0&& n%7==0&&n%3!=0)
             {
                 System.out.println("PlangPlong");
             }
             else if(n%7==0&& n%3==0&&n%5!=0)
             {
                 System.out.println("PlongPling");
             }
             else if(n%3==0&&n%5!=0&&n%7!=0)
             {
                 System.out.println("Pling"); 
             }
              else if(n%3!=0&&n%5==0&&n%7!=0)
             {
                 System.out.println("Plang"); 
             }
              else if(n%3!=0&&n%5!=0&&n%7==0)
             {
                 System.out.println("Plong"); 
             }
             else{
                 System.out.print(n);
             }
             
        }
        
        }

---
+++ RainDrop.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class k{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+          if(n%3==0&&n%5==0&&n%7!=0)
+         {
+             System.out.println("PlingPlang"); 
+         }
+         else if(n%5==0&& n%7==0&&n%3!=0)
+         {
+             System.out.println("PlangPlong");
+         }
+         else if(n%7==0&& n%3==0&&n%5!=0)
+         {
+             System.out.println("PlongPling");
+         }
+         else if(n%3==0&&n%5!=0&&n%7!=0)
+         {
+             System.out.println("Pling"); 
+         }
+          else if(n%3!=0&&n%5==0&&n%7!=0)
+         {
+             System.out.println("Plang"); 
+         }
+          else if(n%3!=0&&n%5!=0&&n%7==0)
+         {
+             System.out.println("Plong"); 
+         }
+         else{
+             System.out.print(n);
+         }
+         
+    }
+    
+    }
