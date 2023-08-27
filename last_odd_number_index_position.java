fea2c15fc9d933f2bee79e3c466f4a4250b5e9db
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2023-08-27 20:14:58 +0530

    import java.util.Scanner; public class K{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int m=0;         for(int i=0;i<n;i++)         {             if(a[i]%2!=0)             {                 m=i;             }         }         System.out.println(m);
        }
    }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class K{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int m=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2!=0)
+            {
+                m=i;
+            }
+        }
+        System.out.println(m);
+        
+    }
+}
