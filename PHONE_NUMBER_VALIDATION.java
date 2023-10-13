174c38e6271cca51aac693027974ff9e7872dc55
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2023-10-13 11:46:07 +0530

    import java.util.*; public class K{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String n=sc.next();        // String s=Integer.toString(n);          if(n.length()==10){              System.out.println("Valid");          }          else{              System.out.println("Invalid");          }         }     }

---
+++ PHONE_NUMBER_VALIDATION.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class K{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+       // String s=Integer.toString(n);
+         if(n.length()==10){
+             System.out.println("Valid");
+         }
+         else{
+             System.out.println("Invalid");
+         }
+        }
+    }
