894faad67da89028da4683f671fa7ea4b4be9bda
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2023-10-13 10:46:28 +0530

    import java.util.Scanner; public class K{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();          int q=n;          int r,s=0;         while(q!=0){             r=q%10;             s=s*10+r;             q=q/10;         }         if(s==n){         System.out.println("True");}         else{             System.out.println("False");         }     } }

--- Palindrome.java
+++ Palindrome.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
-public class k{
-    public static void main(String[] args)
-{
-    Scanner sc=new Scanner(System.in);
-    int n,r,s=0;
-    n=sc.nextInt();
-    int q=n;
-    while(q!=0){    
-       r=q%10;
-       q=q/10;
-       s=s*10+r;
-     }
-     if(n==s){
-     System.out.println("Palindrome");}
-     
-
-else{
-    System.out.println("Not Palindrome");
-    
-}
+public class K{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int q=n;
+         int r,s=0;
+        while(q!=0){
+            r=q%10;
+            s=s*10+r;
+            q=q/10;
+        }
+        if(s==n){
+        System.out.println("True");}
+        else{
+            System.out.println("False");
+        }
     }
 }
