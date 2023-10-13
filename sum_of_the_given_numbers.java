3532907eecfec3a359ab122f5a4b67e5ea619742
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2023-10-13 11:48:14 +0530

    import java.util.Scanner; public class K{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++){             int a=sc.nextInt();             int b=sc.nextInt();             int sum=0;             sum=a+b;             System.out.println(sum);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class K{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int sum=0;
+            sum=a+b;
+            System.out.println(sum);
+        }
+    }
+}
