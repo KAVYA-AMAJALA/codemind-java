069414855f971ef5f1d0caa99d7030175b36e4b1
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2023-10-13 11:26:27 +0530

    import java.util.Scanner; public class K{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int p=0;         for(int i=1;i<=n/2;i++){             int s=i*i;             if(n==s){                p=1;             }         }         if(p==1){             System.out.println("True");         }         else{             System.out.println("False");         }     } }

--- perfect_square_root_or_not.java
+++ perfect_square_root_or_not.java
@@ -1,28 +1,20 @@
 import java.util.Scanner;
-public class k{
-    public static void main(String[] args)
-    {
+public class K{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int m=1,p=0;
-        for(int i=1;i<n;i++)
-        {
-            m=i*i;
-        
-        if(m==n)
-        {
-           p=1;
+        int p=0;
+        for(int i=1;i<=n/2;i++){
+            int s=i*i;
+            if(n==s){
+               p=1;
+            }
         }
-        }
-        if(p==0)
-        {
-            System.out.println("False");
-        }
-        else{
+        if(p==1){
             System.out.println("True");
         }
-        
-          
-       
+        else{
+            System.out.println("False");
+        }
     }
 }
